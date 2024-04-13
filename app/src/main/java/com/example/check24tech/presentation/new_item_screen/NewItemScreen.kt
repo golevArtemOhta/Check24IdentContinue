4de4a542d9901baaa81abc38c78981cc1ee50efd
golevArtemOhta <93866339+golevArtemOhta@users.noreply.github.com>
2024-04-13 16:44:27 +0200

    1/ did title obligatory

--- app/src/main/java/com/example/check24tech/presentation/new_item_screen/NewItemScreen.kt
+++ app/src/main/java/com/example/check24tech/presentation/new_item_screen/NewItemScreen.kt
@@ -4,20 +4,25 @@ import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
 import android.net.Uri
+import android.provider.MediaStore
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
@@ -34,6 +39,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ColorMatrix
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -68,6 +75,7 @@ fun NewItemScreen(
     var textPrice by remember { mutableStateOf("0.0") }
     var editItem by remember { mutableStateOf(false) }
     var capturedImageUri by remember { mutableStateOf<Uri>(Uri.EMPTY) }
+    val isTitleEmpty = textTitle.isEmpty()
 
     val state = rememberScrollState()
 
@@ -130,7 +138,9 @@ fun NewItemScreen(
                 },
                 label = {
                     Text(text = stringResource(id = R.string.title))
-                }
+                },
+                isError = isTitleEmpty,
+                singleLine = true
             )
             Spacer(modifier = Modifier.height(20.dp))
             OutlinedTextField(
@@ -157,62 +167,115 @@ fun NewItemScreen(
             )
         }
 
-        Image(
-            modifier = Modifier
-                .size(100.dp)
-                .clickable {
-                    val permissionCheckResult =
-                        ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
-                    if (permissionCheckResult == PackageManager.PERMISSION_GRANTED) {
-                        cameraLauncher.launch(uri)
-                        viewModel.putEnteredData()
+        if (capturedImageUri.path?.isNotEmpty() == true){
+            Row {
+                Image(
+                    modifier = Modifier
+                        .size(100.dp)
+                        .clickable {
+                            val permissionCheckResult =
+                                ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
+                            if (permissionCheckResult == PackageManager.PERMISSION_GRANTED) {
+                                cameraLauncher.launch(uri)
+                                viewModel.putEnteredData()
+                            } else {
+                                // Request a permission
+                                permissionLauncher.launch(Manifest.permission.CAMERA)
+                            }
+                        },
+                    painter = if (capturedImageUri.path?.isNotEmpty() == true) {
+                        rememberImagePainter(capturedImageUri)
                     } else {
-                        // Request a permission
-                        permissionLauncher.launch(Manifest.permission.CAMERA)
-                    }
+                        painterResource(id = R.drawable.ic_photo_camera)
+                    },
+                    contentDescription = stringResource(id = R.string.photo)
+                )
+                Image(
+                    modifier = Modifier
+                        .size(100.dp)
+                        .clickable {
+                            val permissionCheckResult =
+                                ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
+                            if (permissionCheckResult == PackageManager.PERMISSION_GRANTED) {
+                                cameraLauncher.launch(uri)
+                                viewModel.putEnteredData()
+                            } else {
+                                // Request a permission
+                                permissionLauncher.launch(Manifest.permission.CAMERA)
+                            }
+                        },
+                    painter = if (capturedImageUri.path?.isNotEmpty() == true) {
+                        rememberImagePainter(capturedImageUri)
+                    } else {
+                        painterResource(id = R.drawable.ic_photo_camera)
+                    },
+                    contentDescription = stringResource(id = R.string.photo),
+                    colorFilter = ColorFilter.colorMatrix(ColorMatrix().apply { setToSaturation(0f) })
+                )
+            }
+        } else{
+            Image(
+                modifier = Modifier
+                    .size(100.dp)
+                    .clickable {
+                        val permissionCheckResult =
+                            ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
+                        if (permissionCheckResult == PackageManager.PERMISSION_GRANTED) {
+                            cameraLauncher.launch(uri)
+                            viewModel.putEnteredData()
+                        } else {
+                            // Request a permission
+                            permissionLauncher.launch(Manifest.permission.CAMERA)
+                        }
+                    },
+                painter = if (capturedImageUri.path?.isNotEmpty() == true) {
+                    rememberImagePainter(capturedImageUri)
+                } else {
+                    painterResource(id = R.drawable.ic_photo_camera)
                 },
-            painter = if (capturedImageUri.path?.isNotEmpty() == true) {
-                rememberImagePainter(capturedImageUri)
-            } else {
-                painterResource(id = R.drawable.ic_photo_camera)
-            },
-            contentDescription = stringResource(id = R.string.photo),
-        )
+                contentDescription = stringResource(id = R.string.photo)
+            )
+        }
+
+
         Button(
             onClick = {
-                if (editItem) {
-                    viewModel.updateSaleItem(
-                        SaleItemModel(
-                            id = saleItemId,
-                            title = textTitle,
-                            description = textDescription.takeUnless { it.isNullOrEmpty() }
-                                ?: context.resources.getString(R.string.without_description),
-                            price = if (textPrice.isNotBlank()) textPrice.toDouble() else 0.0,
-                            image = capturedImageUri
+                if (!isTitleEmpty) {
+                    if (editItem) {
+                        viewModel.updateSaleItem(
+                            SaleItemModel(
+                                id = saleItemId,
+                                title = textTitle,
+                                description = textDescription.takeUnless { it.isNullOrEmpty() }
+                                    ?: context.resources.getString(R.string.without_description),
+                                price = if (textPrice.isNotBlank()) textPrice.toDouble() else 0.0,
+                                image = capturedImageUri
+                            )
                         )
-                    )
-                } else {
-                    val isFirstItemAdded = PreferensHelper.isFirstItemAdded(context = context)
-                    if (!isFirstItemAdded){
-                        PreferensHelper.setFirstItemAdded(context = context, true)
+                    } else {
+                        val isFirstItemAdded = PreferensHelper.isFirstItemAdded(context = context)
+                        if (!isFirstItemAdded) {
+                            PreferensHelper.setFirstItemAdded(context = context, true)
+                        }
+                        viewModel.insertSaleItem(
+                            SaleItemModel(
+                                id = 0,
+                                title = textTitle,
+                                description = textDescription.takeUnless { it.isNullOrEmpty() }
+                                    ?: context.resources.getString(R.string.without_description),
+                                price = if (textPrice.isNotBlank()) textPrice.toDouble() else 0.0,
+                                image = if (capturedImageUri.path?.isNotEmpty() == true) {
+                                    capturedImageUri
+                                } else {
+                                    context.getResourceUri(R.drawable.ic_photo_camera)
+                                }
+                            )
+                        )
                     }
-                    viewModel.insertSaleItem(
-                        SaleItemModel(
-                            id = 0,
-                            title = textTitle,
-                            description = textDescription.takeUnless { it.isNullOrEmpty() }
-                                ?: context.resources.getString(R.string.without_description),
-                            price = if (textPrice.isNotBlank()) textPrice.toDouble() else 0.0,
-                            image = if (capturedImageUri.path?.isNotEmpty() == true){
-                                capturedImageUri
-                            } else{
-                                context.getResourceUri(R.drawable.ic_photo_camera)
-                            }
-                        )
-                    )
+                    navigator.navigate(ItemsScreenDestination)
                 }
-                navigator.navigate(ItemsScreenDestination)
             },
+            enabled = !isTitleEmpty,
             colors = ButtonDefaults.buttonColors(Color.Blue)
         ) {
             Text(text = stringResource(id = R.string.save))
